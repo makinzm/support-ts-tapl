77f160078cea788fccf6854ae70219b4f517991b
makinzm <nozomi.maki.da@gmail.com>
2025-06-18 22:04:49 +0900

    wrong

--- book/tmp.ts
+++ book/tmp.ts
@@ -1,6 +1,15 @@
-import { parseObj } from "npm:tiny-ts-parser";
+import { parse, parseObj } from "npm:tiny-ts-parser";
+import { typecheck } from "npm:typecheckers/obj";
 
 const node = parseObj("({a: 10, a: 'string'})")
 
 console.log(node);
 
+const edgeCase = `
+const f = (x: {a : number, a: boolean}) => 2;
+const x = {x: 1, x: true};
+const y = {x: true, x: 1};
+f(x);
+`
+
+console.log(typecheck(parse(edgeCase)));
